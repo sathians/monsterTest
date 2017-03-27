14159c13921e39eb2b0da5003e84fe331c57ce75
sathians <sathian@student.chalmers.se>
2017-03-27 15:04:39 +0200

    hahaha

---
+++ src/main/java/Monster.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Sathian on 2017-03-23.
+ */
+
+import lombok.*;
+
+@Data public class Monster {
+
+    private int numberOfLegs;
+    private String Color;
+    private int dangerLevel;
+
+
+}
