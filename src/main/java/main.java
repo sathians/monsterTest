830c98a1518b492cf9e8c664d1d3247bb7589460
sathians <sathian@student.chalmers.se>
2017-03-27 15:04:03 +0200

    hahaha

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Sathian on 2017-03-23.
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+        Monster invador = new Monster();
+
+        invador.getNumberOfLegs();
+
+    }
+
+}
